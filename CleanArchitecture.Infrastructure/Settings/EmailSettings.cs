3f7eb7d82729586a5b9e02410471501cbb0e7fa9
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-07-04 01:08:16 +0300

    refactoring infrastructure layer

---
+++ CleanArchitecture.Infrastructure/Settings/EmailSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Infrastructure.Settings
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; } = default!;
+        public int Port { get; set; }
+    }
+}
