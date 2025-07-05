66bb9e2892b6464fda27eae036fb9f608a86db28
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-07-05 15:05:19 +0300

    added assembly reference to all layer

---
+++ CleanArchitecture.Domain/AssemblyReference.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Domain;
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+}
---
+++ CleanArchitecture.Infrastructure/AssemblyReference.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Infrastructure;
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+}
