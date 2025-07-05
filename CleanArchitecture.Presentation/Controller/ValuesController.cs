1a14459af65a3a9cee94fc4694d53eb10608c2c8
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-07-05 15:50:58 +0300

    added reference for controller on program.cs

---
+++ CleanArchitecture.Presentation/Controller/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = new[] { "Value1", "Value2", "Value3" };
+            return Ok(values);
+        }
+    }
+}
