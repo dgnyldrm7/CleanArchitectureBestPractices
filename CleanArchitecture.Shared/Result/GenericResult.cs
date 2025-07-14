88a9060b4e34feb64c284253a6a39dd65b3527e2
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-07-14 22:49:37 +0300

    added result pattern

---
+++ CleanArchitecture.Shared/Result/GenericResult.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Shared.Result;
+using System.Net;
+
+namespace CleanArchitecture.Shared.ResultPattern
+{
+    public class GenericResult<T> where T : class
+    {
+        private GenericResult() : this(true) {}
+
+        private GenericResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+
+            if(!isSuccess)
+            {
+                Data = null;
+            }
+        }
+
+        public bool IsSuccess { get; set; }
+        public string? ErrorMessage { get; set; }
+        public T? Data { get; set; }
+        public ProblemDetails? ProblemDetails { get; private init; } = new();
+
+        public static GenericResult<T> Success(T data)
+        {
+            return new GenericResult<T>(true)
+            { 
+                Data = data 
+            };
+        }
+
+        public static GenericResult<T> Failure(string errorMessage)
+        {
+            return new GenericResult<T>(false)
+            { 
+
+                ProblemDetails = new ProblemDetails
+                {
+                    Status = (int)HttpStatusCode.BadRequest,
+                    Title = "Bad Request",
+                    Detail = errorMessage
+                },
+
+                ErrorMessage = errorMessage 
+            };
+        }
+    }
+
+}
---
+++ CleanArchitecture.Shared/Result/ProblemDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Shared.Result
+{
+    public class ProblemDetails
+    {
+        public string Status { get; set; } = "500";
+        public string Title { get; set; } = "Internal Server Error";
+        public string Detail { get; set; } = "An error occurred while processing your request.";
+    }
+}
