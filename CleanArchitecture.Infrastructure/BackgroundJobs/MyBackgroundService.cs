4b9c5c3db99a38e384720d4ad842ff1f3d856d0a
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-07-20 01:47:50 +0300

    added background service

---
+++ CleanArchitecture.Infrastructure/BackgroundJobs/MyBackgroundService.cs
@@ -0,0 +1,28 @@
+﻿namespace CleanArchitecture.Infrastructure.BackgroundJobs
+{
+    public class MyBackgroundService : BackgroundService
+    {
+        private readonly ILogger<MyBackgroundService> _logger;
+
+        public MyBackgroundService(ILogger<MyBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("MyBackgroundService is starting. FROM CleanArcitecture.Infrastructre.BackgroundJobs");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Your background job logic here
+                
+                _logger.LogInformation("MyBackgroundService is running. FROM CleanArcitecture.Infrastructre.BackgroundJobs");
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken); // Adjust the delay as needed
+            }
+
+            _logger.LogInformation("MyBackgroundService is stopping. FROM CleanArcitecture.Infrastructre.BackgroundJobs");
+        }
+    }
+}
--- CleanArchitecture.Infrastructure/GlobalUsings.cs
+++ CleanArchitecture.Infrastructure/GlobalUsings.cs
@@ -8,3 +8,5 @@ global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.Extensions.Options;
 global using System;
 global using System.Threading.Tasks;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
